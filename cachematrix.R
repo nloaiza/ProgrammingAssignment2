5f1eda20256679a6fa170c1412a924a68d11a905
nloaiza <nloaizav@gmail.com>
2014-07-21 20:38:36 -0600

    cachematrix
    assigment2 R programing Coursera

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,54 @@
-## Put comments here that give an overall description of what your
-## functions do
+## This function creates a special "matrix" object that can cache its inverse
 
-## Write a short comment describing this function
+
+
 
 makeCacheMatrix <- function(x = matrix()) {
+  
+  
+    m <- NULL
+    set <- function(y) {
+      x <<- y
+      m <<- NULL
+    }
+    get <- function() x
+    setsolve <- function(solve) m <<- solve
+    getsolve <- function() m
+    list(set = set, get = get,
+         setsolve = setsolve,
+         getsolve = getsolve)
+  }
 
 }
 
 
-## Write a short comment describing this function
+## This function computes the inverse of the special "matrix" 
+## returned by makeCacheMatrix. If the inverse has already 
+## been calculated (and the matrix has not changed),
+## then cacheSolve should retrieve the inverse from the cache.
 
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
+  m <- x$getsolve()
+  if(!is.null(m)) {
+    message("getting cached data")
+    return(m)
+  }
+  data <- x$get()
+  m <- solve(data, ...)
+  x$setsolve(m)
+  m
 }
+
+###testing
+#matrix <- rbind(c(4, 3), c(3, 2)) 
+#m <- makeCacheMatrix(matrix)
+
+# m$get()
+#[,1] [,2]
+#[1,]    4    3
+#[2,]    3    2
+# cacheSolve(m)
+#[,1] [,2]
+#[1,]   -2    3
+#[2,]    3   -4
